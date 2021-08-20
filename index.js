38e731b1d35c1c42c9093e8ac3a0873d2c89ee48
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-20 13:49:02 -0400

    commit tree cd34184a93bf3aa7bc0d387e2e671e1a05087396

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            let newUserInfo = document.createElement('div')
+            let newUserName = document.createElement('div')
+            let newUserEmail = document.createElement('div')
+            let newUserId = document.createElement('div')
+
+            newUserName.textContent = json.name
+            newUserEmail.textContent = json.email
+            newUserId.textContent = json.id
+
+            newUserInfo.append(newUserName, newUserEmail, newUserId)
+            document.querySelector('body').append(newUserInfo)
+        })
+        .catch(error => {
+            document.querySelector('body').append(error.message);
+        });
+
+
+};
+
